24dabe05cb8938e144a0c7e9ac1c23cf370ea732
vijay6390 <vijaydvk1990@gmail.com>
2022-11-02 23:24:28 +0530

    baseurl cahnge

--- src/services/axios-instance.js
+++ src/services/axios-instance.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://localhost:4000/`,
+  // baseURL: `http://localhost:4000/`,
+  baseURL: `https://financier.herokuapp.com/`,
 });
